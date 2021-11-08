952386a64122fa6d0dfeef44dd4257ffddc2c33c
Guilherme Francisco <guifreitas89@hotmail.com>
2021-11-08 14:27:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from ble_motor_ctrl import application
 import json
+from epics import caget
 
 with open("config/config.json", "r") as config_file:
     config = json.load(config_file)
+    
+pvs = []
 
-application.register(config.get("pvs"), config.get("name"))
+for pv in config.get("pvs"):
+    if caget(pv, timeout=1) is not None:
+        pvs.append(pv)
+
+application.register(pvs, config.get("name"))
